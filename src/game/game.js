007f6670199d0d030f82ebc3c59de46420a33015
Iarlhy <iarlhyssomtrue@hotmail.com>
2026-08-05 21:59:53 -0300

    corrigir a sobreposicao de letras na criacao.

--- src/game/game.js
+++ src/game/game.js
@@ -1,53 +1,104 @@
 function the_game(){
-                /*CAPTURA A DIV ONDE AS LETRAS SERAO CRIADAS*/
-                const container = document.getElementById('gameDiv');
-                /*STRING CONTENDO O ALFABETO*/
-                list_lyrics = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                /*VELOCIDADE BASE DE QUEDA DOS BLOCOS*/
-                const velocity_pxS = 120;
-                /*MEDE A ALTURA TOTAL DA JANELA*/
-                const height_background = window.innerHeight;
+    let construtor = true
+    /*CAPTURA A DIV ONDE AS LETRAS SERAO CRIADAS*/
+    const container = document.getElementById('gameDiv');
+    /*STRING CONTENDO O ALFABETO*/
+    list_lyrics = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    /*VELOCIDADE BASE DE QUEDA DOS BLOCOS*/
+    const velocity_pxS = 120;
+    /*MEDE A ALTURA TOTAL DA JANELA*/
+    const height_background = window.innerHeight;
+    let loteinGame = document.querySelectorAll('.box-style');
 
-                /*LOOP DE CRIAÇÃO DOS BLOCOS DE LETRAS E ANIMAÇÃO*/
-                for (let i = 0; i < 26; i++){
-                    /*CRIA UMA NOVA TAG DIV NA MEMORIA DA MAQUINA*/
-                    const novoBox = document.createElement('div')
-                    /*SORTEIA UM NUMERO BASEADO NA LISTA*/
-                    target_index = Math.floor(Math.random()*list_lyrics.length);
-                    /*PEGA A LETRA CORRESPONDENTE AO NUMERO SORTEADO*/
-                    target_lyric = list_lyrics[target_index];
-                    
-                    /*INSERE A LETRA SORTEADA NO ELEMENTO*/
-                    novoBox.textContent = target_lyric;
-                    /*APLICA O ESTILO AO ELEMENTO*/
-                    novoBox.classList.add('box-style')
-                    
-                    /*MEDE A LARGURA TOTAL DA JANELA DO NAVEGADOR E SUBTRAI 60PX (LARGURA DO ELEMENTO)*/
-                    let width_window = window.innerWidth - 60;
-                    /*SORTEIA UMA POSIÇÃO HORIZONTAL ALEATORIA DENTRO DA JANELA*/
-                    let posX = Math.floor(Math.random()*width_window);
-                    /*APLICA NO ESTILO INLINE (LEFT: XPX), ALOCANDO O ELEMENTO NA TELA EM LINHA*/
-                    novoBox.style.left = posX + 'px';
-                    
-                    /*SORTEIA UM NUMERO ENTRE 0 E 399PX E SUBTRAI 500PX*/
-                    let posY = Math.floor(Math.random()*400)-500;
-                    /*ORGANIZA O ELEMENTO EM UM ESPAÇO ACIMA DA TELA VISIVEL*/
-                    novoBox.style.top = posY + 'px';
+    if (loteinGame.length == 0){
+        construtor = true
+    }
+    else{
+        let lotecheck = []
+        for (let i = 0; i < loteinGame.length; i++){
+            let itemStyle = window.getComputedStyle(loteinGame[i])
+            let itemY = parseInt(itemStyle.top, 10);
+            lotecheck.push(itemY)
+        }
+        construtor = lotecheck.every(item => item > 0);
+    }
 
-                    /*SORTEIA UM NUMERO DECIMAL ENTRE 0 E 6 SEGUNDOS*/
-                    let slow = Math.random() * 12;
-                    /*MODIFICA O DELAY DA ANIMAÇÃO DO ELEMENTO*/
-                    novoBox.style.animationDelay = slow + 's';
+    if (construtor == true){
+        construtor = false;
+        let positions = generate_spawnMap();
 
-                    /*CALCULA A DISTANCIA REAL EM PIXELS DA POSIÇÃO Y DO ELEMENTO ATE O FINAL DA TELA VISIVEL*/
-                    let space_down = height_background - posY;
-                    /*CALCULA O TEMPO DE QUEDA BASEADA NA DISTANCIA*/
-                    let timeDrop = space_down / velocity_pxS;
+        /*LOOP DE CRIAÇÃO DOS BLOCOS DE LETRAS E ANIMAÇÃO*/
+        for (let i = 0; i < list_lyrics.length; i++){
+            /*CRIA UMA NOVA TAG DIV NA MEMORIA DA MAQUINA*/
+            const novoBox = document.createElement('div');
+            /*SORTEIA UM NUMERO BASEADO NA LISTA*/
+            target_index = Math.floor(Math.random()*list_lyrics.length);
+            /*PEGA A LETRA CORRESPONDENTE AO NUMERO SORTEADO*/
+            target_lyric = list_lyrics[target_index];
+            
+            /*INSERE A LETRA SORTEADA NO ELEMENTO*/
+            novoBox.textContent = target_lyric;
+            /*APLICA O ESTILO AO ELEMENTO*/
+            novoBox.classList.add('box-style');
+            
+            position_index = Math.floor(Math.random()*positions.length);
+            target_position = positions[position_index];
+            /*console.log(position_index+" index");
+            console.log(positions.length+" array length");
+            console.log(target_position+" Tg Position");*/
 
-                    /*DEFINE A VELOCIDADE DE QUEDA DO ELEMENTO*/
-                    novoBox.style.animationDuration = timeDrop + 's';
+            /*POSIÇÃO HORIZONTAL ALEATORIA DENTRO DA JANELA*/
+            let posX = target_position["x"];
 
-                    /*INSERE O ELEMENTO COMO CHILD DA DIV CONTAINER*/
-                    container.appendChild(novoBox);
-                };
-            };
+            /*ALOCANDO O ELEMENTO AO EIXO X*/
+            novoBox.style.left = posX + 'px';
+            
+            /*POSIÇÃO VERTICAL ALEATORIA DENTRO DA JANELA*/
+            let posY = target_position["y"];
+
+            /*ALOCANDO O ELEMENTO AO EIXO Y*/
+            novoBox.style.top = posY + 'px';
+
+            /*CALCULA A DISTANCIA REAL EM PIXELS DA POSIÇÃO Y DO ELEMENTO ATE O FINAL DA TELA VISIVEL*/
+            let space_down = height_background - posY;
+            /*CALCULA O TEMPO DE QUEDA BASEADA NA DISTANCIA*/
+            let timeDrop = space_down / velocity_pxS;
+
+            /*DEFINE A VELOCIDADE DE QUEDA DO ELEMENTO*/
+            novoBox.style.animationDuration = timeDrop + 's';
+
+            /*INSERE O ELEMENTO COMO CHILD DA DIV CONTAINER*/
+            container.appendChild(novoBox);
+            positions.splice(position_index,1);
+        };
+        /*console.log(positions.length+" array cont final")*/
+        positions = []
+        loteinGame = document.querySelectorAll('.box-style');
+
+    }
+};
+
+function generate_spawnMap(){
+    const windowHeight = window.innerHeight; /*Y*/
+    const windowWidth = window.innerWidth;   /*X*/
+    const boxSize = {"x":60,"y":60}
+    const positions = []
+    let contSpacesX = windowWidth / boxSize["x"]
+    let contSpacesY = Math.floor(windowHeight / boxSize["y"])
+    
+    for (let c = 0; c < contSpacesX; c++){
+        let sortX = c * 60
+        for (let r = 1; r < contSpacesX; r++){
+            let sortY = (r * 60)*-1
+            let position = {"x":sortX,"y":sortY}
+            positions.push(position)
+        }
+    }
+
+    /*console.log(contSpacesX)
+    console.log(windowWidth)
+    console.log(contSpacesY)
+    console.log(windowHeight)
+    console.log(positions)*/
+    return positions
+};
